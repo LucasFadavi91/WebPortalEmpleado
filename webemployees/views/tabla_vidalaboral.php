b166d4c2f9e397f837f0e7d2dfc1a54bc6941ff2
LucasFadavi91 <71818752+LucasFadavi91@users.noreply.github.com>
2021-02-25 10:36:06 +0100

    Add files via upload

---
+++ webemployees/views/tabla_vidalaboral.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <meta charset="utf-8" />
+
+    <style>
+        table{
+             background-color: whitesmoke; 
+        }
+        body{
+           background-color: royalblue; 
+        }
+        .formulario{
+    padding: 10px;
+    
+}
+
+label{
+    display: inline-block;
+    width: 120px;
+}
+    </style>
+</head>
+<body>
+
+
+<a href="../controllers/controller_vidalaboral.php"><button>Volver</button></a>
+
+        <h3> Información del empleado </h3>    
+     
+<?php    
+
+
+    echo "<br><br><table border='1' cellpadding='3'>";
+    echo "<tr>";
+        echo "<th>"."Nombre"."</th>";
+        echo "<th>"."Apellidos"."</th>";
+        echo "<th>"."Fecha de nacimiento"."</th>";
+        echo "<th>"."Genero"."</th>";
+        echo "<th>"."Fecha de contratación"."</th>";
+        echo "<th>"."Salario"."</th>";
+        echo "<th>"."Titulo"."</th>";
+        echo "<th>"."Departamento"."</th>";
+    echo "</tr>";
+
+        foreach($_SESSION['userId'] as $emp_no) {
+            //var_dump($emp); 
+            $birth_date=$emp['birth_date'];
+            $first_name=$emp['first_name'];
+            $last_name=$emp['last_name'];
+            $gender=$emp['gender'];
+            $hire_date=$emp['hire_date'];
+            $salary=$emp['salary'];
+            $title=$emp['title'];
+            $dept_no=$emp['dept_no'];
+
+      
+                echo "<tr>";
+                    echo "<td>".$first_name."</td>";
+                    echo "<td>".$last_name."</td>";
+                    echo "<td>".$birth_date."</td>";
+                    echo "<td>".$gender."</td>";
+                    echo "<td>".$hire_date."</td>";
+                    echo "<td>".$salary."</td>";
+                    echo "<td>".$title."</td>";
+                    echo "<td>".$dept_no."</td>";
+                echo "</tr>";
+        }        
+                echo "</table"; 
+
+?>
+
+<br>
+
+          
+</body>
+</html>
+
